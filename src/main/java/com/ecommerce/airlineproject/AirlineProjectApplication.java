8050cefeba985a4778916975de5d521631924863
Egemen Uner <23070006051@stu.yasar.edu.tr>
2026-03-30 14:24:07 +0300

    debugged

--- src/main/java/com/ecommerce/airlineproject/AirlineProjectApplication.java
+++ src/main/java/com/ecommerce/airlineproject/AirlineProjectApplication.java
@@ -1,8 +1,11 @@
 package com.ecommerce.airlineproject;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "AWS Server ")})
 @SpringBootApplication
 public class AirlineProjectApplication {
 
