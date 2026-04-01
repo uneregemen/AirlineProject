34012149426217361b708e9e729707b94da87cda
Egemen Uner <23070006051@stu.yasar.edu.tr>
2026-04-01 14:07:53 +0300

    add: Swagger configuration formatting

--- src/main/java/com/ecommerce/airlineproject/config/SwaggerConfig.java
+++ src/main/java/com/ecommerce/airlineproject/config/SwaggerConfig.java
@@ -14,24 +14,22 @@ import java.util.List;
 @Configuration
 public class SwaggerConfig {
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info().title("Airline API System").version("v1"))
-                .servers(List.of(
-                        new Server().url("http://16.171.6.106:8081").description("AWS Production Server"),
-                        new Server().url("/").description("Default Server URL")
-                ))
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth")) // TÜM ENDPOINT'LERE TOKEN ŞARTI EKLER!
-                .components(new Components()
-                        .addSecuritySchemes("bearerAuth",
-                                new SecurityScheme()
-                                        .name("bearerAuth")
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")
-                                        .in(SecurityScheme.In.HEADER)
-                        )
-                );
-    }
+        @Bean
+        public OpenAPI customOpenAPI() {
+                return new OpenAPI()
+                                .info(new Info().title("Airline API System").version("v1"))
+                                .servers(List.of(
+                                                new Server().url("http://16.171.6.106:8081")
+                                                                .description("AWS Production Server")))
+                                .addSecurityItem(new SecurityRequirement().addList("bearerAuth")) // TÜM ENDPOINT'LERE
+                                                                                                  // TOKEN ŞARTI EKLER!
+                                .components(new Components()
+                                                .addSecuritySchemes("bearerAuth",
+                                                                new SecurityScheme()
+                                                                                .name("bearerAuth")
+                                                                                .type(SecurityScheme.Type.HTTP)
+                                                                                .scheme("bearer")
+                                                                                .bearerFormat("JWT")
+                                                                                .in(SecurityScheme.In.HEADER)));
+        }
 }
