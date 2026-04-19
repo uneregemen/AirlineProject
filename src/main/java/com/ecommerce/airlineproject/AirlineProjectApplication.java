bcda7bc69bbce610c8bb2a1d022799712af7de73
Egemen Uner <23070006051@stu.yasar.edu.tr>
2026-04-19 23:40:50 +0300

    fix: password

--- src/main/java/com/ecommerce/airlineproject/AirlineProjectApplication.java
+++ src/main/java/com/ecommerce/airlineproject/AirlineProjectApplication.java
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(servers = {@Server(url = "/", description = "AWS Server ")})
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "AWS Server ") })
 @SpringBootApplication
 public class AirlineProjectApplication {
 
